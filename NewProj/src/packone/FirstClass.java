38177fec46a6f482e4b9be6bc0e15ccce97ef1c7
DELL <DELL@DESKTOP-BPC4G7D>
2019-04-07 14:11:28 +0530

    Release2

--- NewProj/src/packone/FirstClass.java
+++ NewProj/src/packone/FirstClass.java
@@ -3,7 +3,8 @@ package packone;
 public class FirstClass {
 
 	public static void main(String[] args) {
-System.out.println("Pass");
+    System.out.println("Pass");
+    System.out.println("Fail");
 	}
 
 }
